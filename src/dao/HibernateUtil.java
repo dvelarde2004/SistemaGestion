dd14b53dd885582a3a5f3e924c6bd7fdf4cfc6b8
Daniel <dvelardecifpfbmol.eu>
2025-12-17 17:15:43 +0100

    actualizacion programa facturacion

---
+++ src/dao/HibernateUtil.java
@@ -0,0 +1,22 @@
+package dao;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class HibernateUtil {
+    private static final SessionFactory sessionFactory;
+
+    static {
+        try {
+            // Busca automáticamente hibernate.cfg.xml en src/
+            sessionFactory = new Configuration().configure().buildSessionFactory();
+        } catch (Throwable ex) {
+            System.err.println("Error al crear SessionFactory: " + ex);
+            throw new ExceptionInInitializerError(ex);
+        }
+    }
+
+    public static SessionFactory getSessionFactory() {
+        return sessionFactory;
+    }
+}
